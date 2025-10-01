652e01d6168e4ae96b06c150563239e899f314ea
артур комков <stenplay33@gmail.com>
2025-10-01 16:18:58 +0500

    Task0Add project files.

---
+++ Tyuiu.KomkovAA.Sprint1.Task0.V26.Test/DataServiseTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task0.V26.Lib;
+namespace Tyuiu.KomkovAA.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataServise ds = new DataServise();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
