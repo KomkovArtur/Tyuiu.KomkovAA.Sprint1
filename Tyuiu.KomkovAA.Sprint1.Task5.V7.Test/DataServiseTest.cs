6fc0a9064e77fc211888e6e9737a19e188ddd0ad
артур комков <stenplay33@gmail.com>
2025-10-02 19:09:57 +0500

    Task5

---
+++ Tyuiu.KomkovAA.Sprint1.Task5.V7.Test/DataServiseTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task5.V7.Lib;
+namespace Tyuiu.KomkovAA.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataServise ds = new DataServise();
+            double a = 150;
+            var res = ds.AngleToHoursMinutes(a);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task5.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataServise ds = new DataServise();
+        Console.Title = "Спринт #0 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #7                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Определить h – полное количество часов прошедших от начала суток до того*");
+        Console.WriteLine("* момента (в первой половине дня), когда часовая стрелка повернулась на f *");
+        Console.WriteLine("* f градусов (0<f<360, f – вещественное число).                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите градусы:");
+        double f = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ответ:");
+        Console.WriteLine(ds.AngleToHoursMinutes(f));
+    }
+}
