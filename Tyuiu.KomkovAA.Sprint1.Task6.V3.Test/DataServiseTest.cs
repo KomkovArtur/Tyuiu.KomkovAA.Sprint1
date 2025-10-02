fb2901e22d4279865951624f63bed6bb877cbffe
артур комков <stenplay33@gmail.com>
2025-10-02 20:05:39 +0500

    Task6

---
+++ Tyuiu.KomkovAA.Sprint1.Task6.V3.Test/DataServiseTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task6.V3.Lib;
+namespace Tyuiu.KomkovAA.Sprint1.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataServise ds = new DataServise();
+
+            string words = "ia lublu lol";
+            string res = ds.LastLetterWord(words);
+            Assert.AreEqual("aul", res);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint1.Task6.V3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task6.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataServise ds = new DataServise();
+        Console.Title = "Спринт #0 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #3                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать строку,       *");
+        Console.WriteLine("* составленную из последних литер всех слов.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите текст:");
+
+        string a = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.LastLetterWord(a);
+
+        Console.WriteLine(res);
+    }
+}
