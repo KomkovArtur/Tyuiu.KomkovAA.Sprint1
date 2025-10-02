63c178675a1932da51e50bc5d36d2da9db46a49b
артур комков <stenplay33@gmail.com>
2025-10-02 18:33:20 +0500

    Task4

---
+++ Tyuiu.KomkovAA.Sprint1.Task4.V22.Test/DataServiseTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task4.V22.Lib;
+namespace Tyuiu.KomkovAA.Sprint1.Task4.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataServise ds = new DataServise();
+            double x = 10.0;
+            double y = 10.0;
+            Assert.AreEqual(0.011, ds.Calculate(x, y));
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint1.Task4.V22/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KomkovAA.Sprint1.Task4.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataServise ds = new DataServise();
+        Console.Title = "Спринт #0 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране. Ответ          *");
+        Console.WriteLine("* округлите до 3 знаков после запятой.                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите числf:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ответ");
+        Console.WriteLine(ds.Calculate(x, y));
+
+    }
+}
